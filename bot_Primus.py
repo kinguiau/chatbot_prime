bde549e2541ee88097b8ef60ba8ecbcfbbda1ac8
kinguiau <meioambienteprojeto1@gmail.com>
2026-03-21 16:50:41 -0300

    v5
    tentativa de integração ao tkinter

--- bot_Primus.py
+++ bot_Primus.py
@@ -20,12 +20,16 @@ grupos = {
  "Nossas espécies unidas por um passado há muito esquecido e aliadas para enfrentar o futuro.\n Eu sou Optimus Prime e deixo esta mensagem para que nossas histórias sejam lembradas.\n Pois nestas memórias seremos eternos":os.path.join(busca, 'Discurso do Optimus prime (Transformers 2) #2.mp3'),
  "Em qualquer guerra, a calmaria vem entre as tempestades.\n Terão dias em que perderemos a fé, dias em que nossos aliados se voltarão contra nós,\n mas nunca chegará o dia em que deixaremos este planeta e seu povo.":os.path.join(busca, 'Discurso do Optimus prime (Transformers 3) #3.mp3'),
  "Existem mistérios no Universo que nunca desvendaremos, mas quem nós somos e por que estamos aqui não faz parte desses segredos,\n as respostas carregamos dentro de nós. Eu sou Optimus Prime, e essa mensagem é para meus criadores:\n deixem o Planeta Terra em paz, porque estou indo pegar vocês.":os.path.join(busca, 'Discurso do Optimus prime (Transformers 4) #4.mp3'),
- "Na essência de todas as lendas existe uma verdade, alguns poucos bravos unidos para salvar seus mundos, podemos ser heróis em nossa própria vida, cada um de nós, se tivermos coragem para tentar. Nossos destinos sempre estiveram interligados, mas agora, nossos mundos nos uniram num só. Temos que concertar nossos planetas e trabalhar juntos se quisermos sobreviver. Um segredo perigoso está enterrado no interior da terra, neste planeta há mais do que os olhos podem ver. Eu sou Optimus Prime, chamando todos os Autobots, está na hora de vir para casa.":os.path.join(busca, 'Discurso do Optimus prime (Transformers 5) #5.mp3')
+ "Na essência de todas as lendas existe uma verdade, alguns poucos bravos unidos para salvar seus mundos,\n podemos ser heróis em nossa própria vida, cada um de nós, se tivermos coragem para tentar.\n Nossos destinos sempre estiveram interligados, mas agora, nossos mundos nos uniram num só.\n Temos que concertar nossos planetas e trabalhar juntos se quisermos sobreviver. Um segredo perigoso está enterrado no interior da terra,\n neste planeta há mais do que os olhos podem ver. Eu sou Optimus Prime,\n chamando todos os Autobots, está na hora de vir para casa.":os.path.join(busca, 'Discurso do Optimus prime (Transformers 5) #5.mp3')
 }
+
 
 Pares = [
   [
-    r"oi|ola|viado",["oi","saudações"]  
+    r"oi|ola|viado|bom dia",["oi","saudações"]  
+  ],
+  [
+    r"uau",["que massa","que legal"]
   ],
   [
     r"qual seu nome?|como você se chama?",["meu nome é PRIMUS","sou denominado PRIMUS"]  
@@ -38,7 +42,7 @@ Pares = [
   ]
   ]
 
-class Allspark:
+class Primus:
     def __init__(self):
         print("carregando", end="", flush=True)
         time.sleep(1)
@@ -74,7 +78,41 @@ class Allspark:
             else:
                 print(chat)
 
+class Unicron:
+  def __init__(self):
+     print("carregando", end="", flush=True)
+     time.sleep(1)
+     print(".", end="", flush=True)
+     time.sleep(0.3)
+     print(".", end="", flush=True)
+     time.sleep(0.3)
+     print(".")
+     print("operando")
 
-# 🔥 EXECUTA SÓ SE RODAR DIRETO
+     self.resposta = Chat(Pares, reflections)
+def megatron(self):
+        while True:
+            try:
+                chat = self.resposta.respond(str(input('-->')))
+
+                if chat in discursos:
+                    print(chat)
+                    pygame.mixer.music.load(grupos[chat])
+                    pygame.mixer.music.play()
+                    while pygame.mixer.music.get_busy():
+                        time.sleep(0.5)
+                    continue
+
+                elif chat in ["olha que dialogo merda!!!"]:
+                    print(chat)
+                    break
+
+            except KeyboardInterrupt:
+                break
+
+            else:
+                print(chat)
+
+
 if __name__ == "__main__":
-    Allspark().prime()
+    Primus().prime()
