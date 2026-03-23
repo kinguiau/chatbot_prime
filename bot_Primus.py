e38498e9148b40acb1bb41cd1b9c66fe124b9af5
kinguiau <meioambienteprojeto1@gmail.com>
2026-03-23 13:13:25 -0300

    v7
    style: barra de carregamento
    fix: correção de um dos discursos na interface gráfica

--- bot_Primus.py
+++ bot_Primus.py
@@ -15,7 +15,7 @@ discursos = [
  "Nossas espécies unidas por um passado há muito esquecido e aliadas para enfrentar o futuro.\n Eu sou Optimus Prime e deixo esta mensagem para que nossas histórias sejam lembradas.\n Pois nestas memórias seremos eternos",
  "Em qualquer guerra, a calmaria vem entre as tempestades.\n Terão dias em que perderemos a fé, dias em que nossos aliados se voltarão contra nós,\n mas nunca chegará o dia em que deixaremos este planeta e seu povo.",
  "Existem mistérios no Universo que nunca desvendaremos, mas quem nós somos e por que estamos aqui não faz parte desses segredos,\n as respostas carregamos dentro de nós. Eu sou Optimus Prime, e essa mensagem é para meus criadores:\n deixem o Planeta Terra em paz, porque estou indo pegar vocês.",
- "Na essência de todas as lendas existe uma verdade, alguns poucos bravos unidos para salvar seus mundos, podemos ser heróis em nossa própria vida, cada um de nós, se tivermos coragem para tentar. Nossos destinos sempre estiveram interligados, mas agora, nossos mundos nos uniram num só. Temos que concertar nossos planetas e trabalhar juntos se quisermos sobreviver. Um segredo perigoso está enterrado no interior da terra, neste planeta há mais do que os olhos podem ver. Eu sou Optimus Prime, chamando todos os Autobots, está na hora de vir para casa."
+ "Na essência de todas as lendas existe uma verdade, alguns poucos bravos unidos para salvar seus mundos,\n podemos ser heróis em nossa própria vida, cada um de nós, se tivermos coragem para tentar.\n Nossos destinos sempre estiveram interligados, mas agora, nossos mundos nos uniram num só.\n Temos que concertar nossos planetas e trabalhar juntos se quisermos sobreviver. Um segredo perigoso está enterrado no interior da terra,\n neste planeta há mais do que os olhos podem ver. Eu sou Optimus Prime,\n chamando todos os Autobots, está na hora de vir para casa."
 ]
 
 grupos = {
@@ -83,6 +83,7 @@ class Primus:
 
 root = tk.Tk()
 root.title("Que desperte Optimus Prime")
+ttk.Style().configure("Barra_Carregar.TLabel", background="blue", foreground="blue")
 frm = ttk.Frame(root, padding=60)
 frm.grid()
 
@@ -90,13 +91,16 @@ class Unicron:
     def __init__(self):
         self.resposta = Chat(Pares, reflections)
         
-        ttk.Label(frm, text="autobots").grid(column=0, row=0)
+        ttk.Label(frm, text="autobots", style="Red.TLabel").grid(column=0, row=0)
 
         self.label = ttk.Label(frm, text="carregando")
         self.label.grid(column=0, row=2)
 
         self.comando = ttk.Label(frm, text="")
         self.comando.grid(column=0, row=5)
+
+        self.barra = ttk.Label(frm, text="", style="Barra_Carregar.TLabel")
+        self.barra.grid(column=0, row=3)
 
         self.resposta_label = ttk.Label(frm, text="")
         self.resposta_label.grid(column=0, row=6)
@@ -112,10 +116,12 @@ class Unicron:
     
     def animar(self, pontos=0):
         if pontos <= 3:
-            self.label.config(text="carregando" + "." * pontos)
+            self.label.config(text="carregando" + "." * pontos, font= 30)
+            self.barra.config(text="_____" * (pontos + 1))
             root.after(500, lambda: self.animar(pontos + 1))
+        
         else:
-            self.label.config(text="operando")
+            self.label.config(text="operando", font=22)
             self.conversa.grid(column=0, row=3)
             self.button.grid(column=0, row=4)
     
@@ -123,18 +129,22 @@ class Unicron:
         texto= self.entrada.get()
         self.comando.config(text="vc: {}".format(texto))
         chat = self.resposta.respond(texto)
+        
         if chat in discursos:
-                    self.resposta_label.config(text=chat)
-                    pygame.mixer.music.load(grupos[chat])
-                    pygame.mixer.music.play()
-                    pygame.mixer.music.get_busy()
+            self.resposta_label.config(text=chat)
+            pygame.mixer.music.load(grupos[chat])
+            pygame.mixer.music.play()
+            pygame.mixer.music.get_busy()
                         
                     
 
         elif chat in ["olha que dialogo merda!!!"]:
-                    self.resposta_label.config(text=chat)
-                    root.after(3000,root.destroy)
+            self.resposta_label.config(text=chat)
+            root.after(3000,root.destroy)
             
+
+        elif chat == None:
+            self.resposta_label.config(text="isso está fora das interações")
 
         else:
             self.resposta_label.config(text=chat)
