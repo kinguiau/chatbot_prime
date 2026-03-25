3ab7f9d9163e0ea8692a14b0a0e8dc1f381cacd0
kinguiau <meioambienteprojeto1@gmail.com>
2026-03-25 08:39:50 -0300

    v8
    style: utilização de camadas, utlização de imagens
    
    doc: discursos movido para arquivo .json

--- bot_Primus.py
+++ bot_Primus.py
@@ -2,7 +2,7 @@ import nltk, pygame,json,time,os
 import tkinter as tk
 from tkinter import ttk
 from nltk.chat.util import Chat, reflections
-
+from PIL import Image, ImageTk
 
 pygame.mixer.init()
 pygame.init()
@@ -10,7 +10,7 @@ busca = os.path.dirname(__file__)
 
 
 
-discursos = [
+"""discursos = [
  "Sem o Allspark não podemos levar a vida de volta ao nosso planeta.\n Então o destino trouxe sua recompensa. Um novo mundo para chamarmos de casa.\n Vivemos entre os humanos agora, sempre escondidos, mas vigiando eles em segredo.\n Esperando, protegendo. Eu testemunhei a capacidade e a coragem deles.\n E embora sejamos de mundos distintos, como nós eles são mais do que os olhos podem ver.\n Eu sou Optimus Prime. E mando esta mensagem para qualquer Autobot sobrevivente que esteja refugiando entre as estrelas.\n Nós estamos aqui. Nós estamos esperando",
  "Nossas espécies unidas por um passado há muito esquecido e aliadas para enfrentar o futuro.\n Eu sou Optimus Prime e deixo esta mensagem para que nossas histórias sejam lembradas.\n Pois nestas memórias seremos eternos",
  "Em qualquer guerra, a calmaria vem entre as tempestades.\n Terão dias em que perderemos a fé, dias em que nossos aliados se voltarão contra nós,\n mas nunca chegará o dia em que deixaremos este planeta e seu povo.",
@@ -24,7 +24,11 @@ grupos = {
  "Em qualquer guerra, a calmaria vem entre as tempestades.\n Terão dias em que perderemos a fé, dias em que nossos aliados se voltarão contra nós,\n mas nunca chegará o dia em que deixaremos este planeta e seu povo.":os.path.join(busca, 'Discurso do Optimus prime (Transformers 3) #3.mp3'),
  "Existem mistérios no Universo que nunca desvendaremos, mas quem nós somos e por que estamos aqui não faz parte desses segredos,\n as respostas carregamos dentro de nós. Eu sou Optimus Prime, e essa mensagem é para meus criadores:\n deixem o Planeta Terra em paz, porque estou indo pegar vocês.":os.path.join(busca, 'Discurso do Optimus prime (Transformers 4) #4.mp3'),
  "Na essência de todas as lendas existe uma verdade, alguns poucos bravos unidos para salvar seus mundos,\n podemos ser heróis em nossa própria vida, cada um de nós, se tivermos coragem para tentar.\n Nossos destinos sempre estiveram interligados, mas agora, nossos mundos nos uniram num só.\n Temos que concertar nossos planetas e trabalhar juntos se quisermos sobreviver. Um segredo perigoso está enterrado no interior da terra,\n neste planeta há mais do que os olhos podem ver. Eu sou Optimus Prime,\n chamando todos os Autobots, está na hora de vir para casa.":os.path.join(busca, 'Discurso do Optimus prime (Transformers 5) #5.mp3')
-}
+}"""
+
+with open(os.path.join(busca, 'text.json'), "r", encoding="utf-8") as f:
+    dados = json.load(f)
+
 
 
 Pares = [
@@ -38,7 +42,7 @@ Pares = [
     r"qual seu nome?|como você se chama?",["meu nome é PRIMUS","sou denominado PRIMUS"]  
   ],
   [
-    r"mande a braba|farme aura|discurso", discursos
+    r"mande a braba|farme aura|discurso", dados["discursos"]
   ],
   [
     r"isso acaba aqui|comunicação encerrada|tchau|quitei|gg ez", ["olha que dialogo merda!!!"]
@@ -63,9 +67,9 @@ class Primus:
             try:
                 chat = self.resposta.respond(str(input('-->')))
 
-                if chat in discursos:
+                if chat in dados["discursos"]:
                     print(chat)
-                    pygame.mixer.music.load(grupos[chat])
+                    pygame.mixer.music.load(dados["grupos"][chat])
                     pygame.mixer.music.play()
                     while pygame.mixer.music.get_busy():
                         time.sleep(0.5)
@@ -84,14 +88,27 @@ class Primus:
 root = tk.Tk()
 root.title("Que desperte Optimus Prime")
 ttk.Style().configure("Barra_Carregar.TLabel", background="blue", foreground="blue")
-frm = ttk.Frame(root, padding=60)
+ttk.Style().configure("Remover_Fundo.TLabel", background="#696969", foreground="black")
+frm = ttk.Frame(root, style='Remover_Fundo.TLabel', padding=60)
 frm.grid()
 
 class Unicron:
     def __init__(self):
         self.resposta = Chat(Pares, reflections)
-        
-        ttk.Label(frm, text="autobots", style="Red.TLabel").grid(column=0, row=0)
+
+
+        caminho = os.path.join(busca, "lilith 1.png")
+
+        img = Image.open(caminho)
+        img = img.resize((200, 120))  # (largura, altura)
+
+        self.img = ImageTk.PhotoImage(img)
+
+        self.imagem = ttk.Label(frm, image=self.img)
+        self.imagem.grid(column=0, row=0)
+        self.imagem.lower()
+
+        ttk.Label(frm, text="autobots").grid(column=0, row=0)
 
         self.label = ttk.Label(frm, text="carregando")
         self.label.grid(column=0, row=2)
@@ -130,9 +147,9 @@ class Unicron:
         self.comando.config(text="vc: {}".format(texto))
         chat = self.resposta.respond(texto)
         
-        if chat in discursos:
+        if chat in dados["discursos"]:
             self.resposta_label.config(text=chat)
-            pygame.mixer.music.load(grupos[chat])
+            pygame.mixer.music.load(os.path.join(busca, dados['grupos'][chat]))
             pygame.mixer.music.play()
             pygame.mixer.music.get_busy()
                         
